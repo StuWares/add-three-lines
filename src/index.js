02456f098944292c14ade2ab839a127eb4a943f9
Stu Wares <stuwares@outlook.com>
2022-04-27 18:43:55 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,214 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth, onAuthStateChanged, signInAnonymously } from "firebase/auth";
+//import { getDatabase, ref, set } from "firebase/database";
+import { doc, 
+        getFirestore, 
+        query, 
+        where, 
+        collection, 
+        getDocs, 
+        orderBy, 
+        limit, 
+        setDoc, 
+        updateDoc, arrayUnion, increment } from "firebase/firestore";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAnQEf9FwA00AZkqrGw3L5FN1lDY0n6Q5o",
+  authDomain: "add-3-lines.firebaseapp.com",
+  projectId: "add-3-lines",
+  storageBucket: "add-3-lines.appspot.com",
+  messagingSenderId: "833846222277",
+  appId: "1:833846222277:web:f5a3a35eac072bbd447fac",
+  measurementId: "G-DZQ5TGRW3D"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+const storyColRef = collection(db, "stories");
+
+
+
+let writerId;
+let storyRef;
+let lastLine;
+let isFinalEntry = false;
+
+
+onAuthStateChanged(auth, user => {
+    console.log(user);
+    if (user != null) {
+        writerId = user.uid;
+        
+        console.log('logged in! uid: ' + writerId);
+        
+
+    } else {
+        console.log('No user');
+    }
+});
+
+// create an anonymous UID if none already exists
+signInAnonymously(auth).then(() => {
+    
+    // signed in
+}).catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log(errorCode, errorMessage);
+});
+
+
+
+
+// find the story ID of the oldest that the user hasn't edited last 
+// don't think the query will do what I want exactly but enough for now! (needing to orderBy lastuseredit first is problematic)
+// I'm also not checking for locked / complete stories!
+async function beginWriting(writerId){
+    document.getElementById("getLines").hidden = true;
+    // line below is for testing purposes only
+    //const q = query(storyColRef, where("lastuseredit", "==", writerId ), orderBy("lastedittime", "desc"), limit(1));
+    const q = query(storyColRef, where("islocked", "==", false), where("iscomplete", "==", false), where("lastuseredit", "!=", writerId ), orderBy("lastuseredit"), orderBy("lastedittime", "desc"), limit(1));
+
+    const querySnapshot = await getDocs(q);
+    console.log(querySnapshot);
+
+    // not sure if this returns null or undefined!
+    if (querySnapshot.size > 0) {
+    // if we've found a valid story, bring in the last line of linetext array
+        querySnapshot.forEach((doc) => {
+            lastLine = doc.data().linetext.at(-1);
+            console.log(lastLine);
+            storyRef = doc.ref;
+
+            // check if this is the final entry
+            if (doc.data().totallines == 33) { 
+                isFinalEntry = true; 
+            } else {
+                isFinalEntry = false;
+            }
+            
+        });
+
+        // lock the story
+        await updateDoc(storyRef, {
+            islocked: true
+        });
+
+        // update DOM with the last line and prompt user to write.
+        document.getElementById("introText").innerHTML = lastLine;
+        document.getElementById("lastLineWritten").innerHTML = "The story so far...";
+
+        if (isFinalEntry) {       
+            document.getElementById('titleText').innerHTML = "You're the final writer! Conclude the story...";
+        } else {
+            document.getElementById('titleText').innerHTML = "Continue the story...";
+        }
+    } else {
+        // start a new story
+        console.log('we need a new story');
+        let createTime = new Date;
+        // add a new doc and save the generated reference id
+        const newStoryRef = doc(collection(db, "stories"));
+        storyRef = newStoryRef;
+    
+        // update the new story with the storyid
+        await setDoc(newStoryRef, {
+            storyid: newStoryRef,
+            lastuseredit: writerId,
+            islocked: true,
+            iscomplete: false,
+            linetext: [],
+            totallines: 0,
+            twitterhandles: [],
+            lastedittime: createTime
+        });
+        document.getElementById("introText").innerHTML = "Let's begin...";
+        document.getElementById("lastLineWritten").innerHTML = " ";
+        document.getElementById('titleText').innerHTML = "It's time to begin a new story!";
+    }
+
+
+};
+    
+
+const startButton = document.getElementById('getLines');
+
+startButton.addEventListener('click', event => {
+    beginWriting(writerId);
+    document.getElementById("textLines").hidden = false;
+    document.getElementById("submitButton").hidden = false;
+});
+
+// Character counters
+const firstLine = document.getElementById('firstLine');
+const secondLine = document.getElementById('secondLine');
+const thirdLine = document.getElementById('thirdLine');
+
+firstLine.addEventListener('keyup', event => {
+    let strCount = firstLine.value.length;
+    document.getElementById('lineOneCount').innerHTML = 100 - strCount;
+});
+secondLine.addEventListener('keyup', event => {
+    let strCount = secondLine.value.length;
+    document.getElementById('lineTwoCount').innerHTML = 100 - strCount;
+});
+thirdLine.addEventListener('keyup', event => {
+    let strCount = thirdLine.value.length;
+    document.getElementById('lineThreeCount').innerHTML = 100 - strCount;
+});
+
+
+// Get the new lines
+const submitButton = document.getElementById("submitButton");
+
+submitButton.addEventListener('click', async event => {
+    
+    const newLineOne = document.getElementById('firstLine').value;
+    const newLineTwo = document.getElementById('secondLine').value;
+    const newLineThree = document.getElementById('thirdLine').value;
+    const twitterId = document.getElementById('twitterHandle').value;
+
+    if (newLineOne == "" || newLineTwo == "" || newLineThree == "") {
+        document.getElementById('titleText').innerHTML = "Please write 3 lines";
+    } else {
+        submitButton.hidden = true;
+
+        const updateTime = new Date;
+
+        await updateDoc(storyRef, {
+            lastuseredit: writerId,
+            islocked: false,
+            iscomplete: isFinalEntry,
+            twitterhandles: arrayUnion(twitterId),
+            linetext: arrayUnion(newLineOne,newLineTwo,newLineThree),
+            totallines: increment(3),
+            lastedittime: updateTime
+        });
+        
+        // hide the entry ui and unhide the submit button
+        document.getElementById("getLines").hidden = false;
+        document.getElementById("getLines").innerHTML= "Let's do another!"
+        document.getElementById("textLines").hidden = true;
+        document.getElementById("instructions").innerHTML = "Thanks for contributing!"
+        document.getElementById("firstLine").value = "";
+        document.getElementById("secondLine").value = "";
+        document.getElementById("thirdLine").value = "";
+        document.getElementById("storySoFar").innerHTML = "";
+        document.getElementById("introText").innerHTML = "";
+        document.getElementById("lastLineWritten").innerHTML = "Lines Submitted!";
+        document.getElementById('lineOneCount').innerHTML = 100;
+        document.getElementById('lineTwoCount').innerHTML = 100;
+        document.getElementById('lineThreeCount').innerHTML = 100;
+
+    }
+
+});
